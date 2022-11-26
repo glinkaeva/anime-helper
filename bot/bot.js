5ec2822ea48835ed282b90a35d234e154428dc07
Glinka Eva <glinka.for.work@gmail.com>
2022-11-26 15:19:29 +0200

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 
 const token = "5878569030:AAFF0xtIv5-uUpUCbXBGuGtcztJprE4woV4";
-const web_link = "https://create-telegram-bot.vercel.app/";
+const web_link = "https://anime-helper-jwc6.vercel.app/";
 
 const bot = new Telegraf(token);
 
